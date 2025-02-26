3f0b7808ccf5cf293932130df17bc79ffc62cf03
Nikhil Mishra <nikhil@Nikhils-MacBook-Air.local>
2025-02-26 18:18:48 +0530

    redesign

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,16 +3,12 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
-import { HeaderComponent } from './components/header/header.component';
-import { FooterComponent } from './components/footer/footer.component';
 import { HomeComponent } from './components/home/home.component';
 
 @NgModule({
   declarations: [
     AppComponent,
-    HeaderComponent,
     HomeComponent,
-    FooterComponent
   ],
   imports: [
     BrowserModule,
